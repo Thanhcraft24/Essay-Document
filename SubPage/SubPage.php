f8f60fed64a7c612ece7eec8d444c48d0d6a3948
Thanhcraft24 <157335652+Thanhcraft24@users.noreply.github.com>
2024-03-12 21:50:20 +0700

    Add files via upload

--- SubPage/SubPage.php
+++ SubPage/SubPage.php
@@ -4,10 +4,11 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="SubPage.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <title>Document</title>
 </head>
 <body>
-<div class='heading'>
+    <div class='heading'>
         <div class='searchMenu'>
             <a href="/MainMenu/MainMenu.php"><img src='/MainMenu/Images/TitleIcon.png' class='titleIcon'></a>
             <div class='search'>
@@ -40,12 +41,13 @@
                     <h3 class='MidSubTitleName'>Example</h3>
                     <h3 class='MidTitleDate'>Date</h3>
                     <div class='ratingBar1'>
+                        <div class='ratingScore'>0</div>
                         <div class='ratingStar'>
-                            <input type='radio' name='rating' class=''>
-                            <input type='radio' name='rating' class=''>
-                            <input type='radio' name='rating' class=''>
-                            <input type='radio' name='rating' class=''>
-                            <input type='radio' name='rating' class=''>
+                            <input type='radio' name='rating'>
+                            <input type='radio' name='rating'>
+                            <input type='radio' name='rating'>
+                            <input type='radio' name='rating'>
+                            <input type='radio' name='rating'>
                         </div>
                     </div>
                     <div class='ratingBar2'></div>
@@ -58,6 +60,64 @@
             <div class='body'>
                 <a href='https://www.youtube.com/watch?v=dQw4w9WgXcQ'><input type='button' value='Xem phim' class='button2'></a>
                 <input type='button' value='Thông tin' class='button2'>
+            </div>
+        </div>
+    </div>
+    <div class='footer'>
+            <div class='subFooter'>
+                <div class='col3'>
+                    <div><img src="/MainMenu/Images/TitleIcon.png"></div>
+                    <div><img src='/MainMenu/Images/logo1.png' class='icon2'></div>
+                    <div><img src='/MainMenu/Images/logo2.png'></div>
+                </div>
+
+                <div class='col4'>
+                    <div href="" class='row1'>Về FPT Play</div>
+                    <div href="" class='row2'>Giới thiệu</div>
+                    <div href="" class='row2'>Các gói dịch vụ</div>
+                    <div href="" class='row2'>Liên hệ</div>
+                    <div href="" class='row2'>Trung tâm hỗ trợ</div>
+                    <div href="" class='row2'>Thông tin</div>
+                </div>
+
+                <div class='col4'>
+                    <div href="" class='row1'>Dịch vụ</div>
+                    <div href="" class='row2'>Gói DATA</div>
+                    <div href="" class='row2'>Quảng cáo</div>
+                    <div href="" class='row2'>Mua gói</div>
+                    <div href="" class='row2'>Bảo hành</div>
+                </div>
+
+                <div class='col4'>
+                    <div href="" class='row1'>Quy định</div>
+                    <div href="" class='row2'>Điều khoản sử dụng</div>
+                    <div href="" class='row2'>Chính sách thanh toán</div>
+                    <div href="" class='row2'>Chính sách bảo mật thông tin dữ liệu</div>
+                </div>
+                <div class='col3'>
+                    <div class='row3'>
+                        <img src='/MainMenu/Images/PhoneIcon.png' class='icon'>
+                        19006600
+                    </div>
+                    <div class='row3'>
+                        <img src='/MainMenu/Images/MailIcon.png' class='mailIcon'>
+                        hotrofptplay.com
+                    </div>
+                    <div class='row3'>Theo dõi chúng tôi trên:</div>
+                    <div class='row3'>
+                        <button class='buttonIcon'><img src='/MainMenu/Images/fbkIcon.png'></button>
+                        <button class='buttonIcon'><img src='/MainMenu/Images/ytbIcon.png'></button>
+                    </div>
+                </div>
+            </div>
+        <div class='subFooter2'>
+            <div class='col5'>
+                <div class='row4'>Công ty Cổ phần Viễn Thông FPT - Người đại diện: Ông Hoàng Việt Anh. Trụ sở: Tầng 2, Tòa nhà FPT Cầu Giấy, Số 17 Phố Duy Tân, Phường Dịch Vọng Hậu, Quận Cầu Giấy, TP.Hà Nội.</div>
+                <div class='row4'>Số giấy chứng nhận đăng ký kinh doanh: 0101778163 do Sở Kế Hoạch Đầu Tư Thành Phố Hà Nội cấp vào ngày 28/07/2005.</div>
+                <div class='row4'>Giấy phép cung cấp Dịch vụ Phát thanh, Truyền hình trên mạng Internet cấp lần đầu ngày 18/11/2016, cấp lại theo Giấy phép số 99/GP-BTTTT ngày 27/03/2023.</div>
+            </div>
+            <div class='deviceIcon'>
+                <img src='/MainMenu/Images/DevicePlatformIcon.png'>
             </div>
         </div>
     </div>
